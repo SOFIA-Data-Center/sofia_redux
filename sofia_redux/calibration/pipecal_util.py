4da509b8afdee7ad480512e62db3d71e96042f0b
ac107586 <benjamin.greiner@irs.uni-stuttgart.de>
2026-08-24 18:46:54 +0200

    Revert AI slop

--- sofia_redux/calibration/pipecal_util.py
+++ sofia_redux/calibration/pipecal_util.py
@@ -802,7 +802,7 @@ def run_photometry(data, header, var, config, **kwargs):
             try:
                 runits = kwargs['runits']
             except KeyError:
-                runits = 'Me/s'
+                runits = 'counts'
             log.info('Source Flux: '
                      '{:.2f} +/- {:.2f} {}'.format(header['STAPFLX'],
                                                    header['STAPFLXE'],
