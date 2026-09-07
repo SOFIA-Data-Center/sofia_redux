dab8e5e832cfc63d2845e9b8f494b1d5b36f3bd1
bryant <aaron.bryant@irs.uni-stuttgart.de>
2026-09-07 16:00:22 +0200

    fix CO names in lookup, coerce wavelengths to floats in get_lines

--- sofia_redux/instruments/fifi_ls/get_lines.py
+++ sofia_redux/instruments/fifi_ls/get_lines.py
@@ -34,4 +34,6 @@ def get_lines():
     names = ['wavelength', 'name']
     df = pandas.read_csv(linefile, comment='#', names=names,
                          sep=r'\t')
+    df['wavelength'] = pandas.to_numeric(df['wavelength'], errors='coerce')
+    df = df.dropna(subset=['wavelength'])
     return list(df['wavelength']), list(df['name'])
