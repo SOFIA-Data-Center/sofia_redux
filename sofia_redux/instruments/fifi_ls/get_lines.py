52a09975fa2fb028a8a880380872e326a3525a07
bryant <aaron.bryant@irs.uni-stuttgart.de>
2026-09-09 11:15:02 +0200

    remove coerce in get lines

--- sofia_redux/instruments/fifi_ls/get_lines.py
+++ sofia_redux/instruments/fifi_ls/get_lines.py
@@ -34,6 +34,4 @@ def get_lines():
     names = ['wavelength', 'name']
     df = pandas.read_csv(linefile, comment='#', names=names,
                          sep=r'\s+')
-    df['wavelength'] = pandas.to_numeric(df['wavelength'], errors='coerce')
-    df = df.dropna(subset=['wavelength'])
     return list(df['wavelength']), list(df['name'])
